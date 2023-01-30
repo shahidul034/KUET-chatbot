fe702e6598d8a3d4958d2cb43d98dcda212d6599
arbitropy <tanim625@gmail.com>
2023-01-31 02:02:59 +0600

    push to main

--- KUETBOT/app.py
+++ KUETBOT/app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk, os, sys
-import requests,re
+import requests, re, random
 
 #pyinstaller C:\Users\Administrator\Documents\Work\KUET-chatbot\KUETBOT\app.py --add-data "C:\Users\Administrator\Documents\Work\KUET-chatbot\KUETBOT\templates;templates" --add-data "C:\Users\Administrator\Documents\Work\KUET-chatbot\KUETBOT\static;static"
 
@@ -25,6 +25,7 @@ sentenceList = nltk.sent_tokenize("demo")
 fullText="demo"
 stopWords="demo"
 sentenceListFirstLine = ["demo"]
+stopWords2 ="demo"
 
 
 #app.route gives the webpage where this functioni will be [triggered],
@@ -35,17 +36,19 @@ def hello_world():
     #nltk.download('punkt')
     sentenceUpdate()
     app.debug = True
-    return render_template('index.html')
+    return render_template('chatbot.html')
 
 #this is called by javascript to get response
 @app.route("/botResponse/<userText>", methods=['GET','POST'])
 def botResponse(userText):
-    bot_response=''
+    botResponse=''
+    if userText == "Random":
+        return RemoveBlock("Here is a random information about KUET:<br>" + random.choice(sentenceList))
     #at first only match the first lines for more accurate search, due to the nature of the dataset
     firstLineFlag,matchIndex=checkBlocks(userText)
     if firstLineFlag==1:
-        bot_response=bot_response+sentenceList[matchIndex]
-        flag,out=searching(bot_response,fullText)
+        botResponse=botResponse+sentenceList[matchIndex]
+        flag,out=searching(botResponse,fullText)
         return out
     else:  
         sentenceList.append(userText)
@@ -58,40 +61,44 @@ def botResponse(userText):
         j=0
         for i in range(len(index)):
             if similarity_scores_list[index[i]]>0.0:
-                bot_response=bot_response+sentenceList[index[i]]
+                botResponse=botResponse+"<br><br>"+sentenceList[index[i]]
                 response_flag=1
                 j+=1
             if j>1:
                 break
         if response_flag==0:
-            bot_response=bot_response+' '+"I apologize, I don't understand."
+            botResponse = BasicConversation(userText)
         else:
-            flag,out=searching(bot_response,fullText)
+            flag,out=searching(botResponse,fullText)
             if flag:
-                bot_response=out
+                botResponse=out
             else:
                 botResponse = RemoveBlock(botResponse)
         sentenceList.remove(userText)
-        return bot_response    
+        return botResponse  
 
-#Updates sentence main sentence list
+def BasicConversation(userInput):
+    return "I apologize, I don't understand."
+
 #Updates sentence main sentence list
 def sentenceUpdate():
     global fullText
     global stopWords
+    global stopWords2
     global sentenceList
     global sentenceListFirstLine
     sentenceListFirstLine.clear()
     
-    url= r'https://raw.githubusercontent.com/shahidul034/KUET-chatbot/main/KUETBOT/static/Software-Project-Data.txt'
-    page = requests.get(url)
-    fullText = page.text
+    # url= r'https://raw.githubusercontent.com/shahidul034/KUET-everything/main/static/Software-Project-Data.txt'
+    # page = requests.get(url)
+    # fullText = page.text
 
-    url= r'https://raw.githubusercontent.com/shahidul034/KUET-chatbot/main/KUETBOT/static/StopWords.txt'
-    page = requests.get(url)
-    stopWords = page.text.split()
-    # fullText = open(r"C:\\Users\Administrator\Documents\Work\KUET-chatbot\KUETBOT\static\Software-Project-Data.txt",encoding="utf8").read()
-    # stopWords = open(r"C:\\Users\Administrator\Documents\Work\KUET-chatbot\KUETBOT\static\StopWords.txt",encoding="utf8").read().split()
+    # url= r'https://raw.githubusercontent.com/shahidul034/KUET-everything/main/static/StopWords.txt'
+    # page = requests.get(url)
+    # stopWords = page.text.split()
+    fullText = open(r"C:\\Users\Administrator\Documents\Work\KUET-everything\static\Software-Project-Data.txt",encoding="utf8").read()
+    stopWords = open(r"C:\\Users\Administrator\Documents\Work\KUET-everything\static\StopWords.txt",encoding="utf8").read().split()
+    stopWords2 = open(r"C:\Users\Administrator\Documents\Work\KUET-everything\static\StopWords2.txt",encoding="utf8").read().split()
     sentenceList = fullText.split("||")
     for block in sentenceList:
         sentenceListFirstLine.append(block.split('\n')[0])
@@ -114,18 +121,23 @@ def index_sort(list_var):
 def checkBlocks(input):
     global sentenceListFirstLine
     sentenceListFirstLine.append(input)
-    cm=CountVectorizer(stop_words="english").fit_transform(sentenceListFirstLine)
-    similarity_scores=cosine_similarity(cm[-1],cm)
+    #creates word vector
+    cm=CountVectorizer(stop_words=stopWords2).fit_transform(sentenceListFirstLine)
+    tempInputVector = cm[-1]
+    #used slicing to remove input text vector to avoid full match duplication
+    cm = cm[:-1,:]
+    #find similarity using cosine distance
+    similarity_scores=cosine_similarity(tempInputVector,cm)
     similarity_scores_list=similarity_scores.flatten()
     index=index_sort(similarity_scores_list)
-    index=index[1:]
-    sentenceListFirstLine.remove(input)
+    sentenceListFirstLine.pop()
     if similarity_scores_list[index[0]]>0.0:
+        print(sentenceListFirstLine[index[0]],file=sys.stderr)
         return 1,index[0]
     else:
         return 0,0
-    
-        
+
+
 #Checks for complete Regular expression blocks
 def searching(botResponse,fullText):
     #search for occurence of block start
@@ -145,15 +157,15 @@ def searching(botResponse,fullText):
             #only starting of a block is found, search for whole block including block end in the fulltext and return
             search2 = re.search(search_string,fullText,re.MULTILINE)
             if search2:
-                print("whole block found",file=sys.stderr)
+                #print("whole block found",file=sys.stderr)
                 msg=search2.group()
                 msg = RemoveBlock(msg)
                 return 1,msg
             else:
-                print("start block found but whole block not found",file=sys.stderr)
+                #print("start block found but whole block not found",file=sys.stderr)
                 return 0,"" 
     else:
-        print("start block not found",file=sys.stderr)
+        #print("start block not found",file=sys.stderr)
         #start block not found, replacing end block if exists
         botResponse=RemoveBlock(botResponse)
         return 1,botResponse
